cc076574b8d520afc4bc3332d27c05df9a407947
Jonas M. Miguel <jonasmmiguel@gmail.com>
2022-10-16 22:45:04 +0000

    [test] multiply

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 def add(x, y):
     """This is an add function"""
-
     return x + y
 
 
+def multiply(x, y):
+    "This multiplies two inputs"
+    ...
+    
+    
 print(add(1, 1))
+print(multiply(2,-1))
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,9 @@
-from hello import add
+from hello import add, multiply
 
 
 def test_add():
     assert 2 == add(1, 1)
+    
+    
+def test_multiply():
+    assert multiply(3,-2) == -6
